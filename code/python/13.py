aee4cea27113fdda4840e7280834cb2f06868253
Kimmin0711 <easy000711@gmail.com>
2025-01-09 16:21:27 +0900

    list 내포

--- code/python/13.py
+++ code/python/13.py
@@ -1,4 +1,4 @@
-'''리스트 내포
+# 리스트 내포
 # : 파이썬에서 리스트를 간결하게 생성하는 문법
 numbers = [1, 2, 3, 4, 5]
 squares = [n**2 for n in numbers]
@@ -10,7 +10,7 @@ print(squares2)
 # 조건문과 함께 사용
 even_squares = [x**2 for x in range(1, 6) if x % 2 == 0]
 print(even_squares)
-'''
+
 
 # 실습
 # 구구단 만들기
